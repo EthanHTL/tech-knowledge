fa92355bb69d6dac23976fb184dda76874d75669
H T L <2388092952@qq.com>
2024-06-19 16:33:22 +0800

    feat: update

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -2,9 +2,6 @@ import { sidebar } from "vuepress-theme-hope";
 
 export const zhSidebar = sidebar({
   "/": [],
-  // "/posts/": 'structure',
   "/cookbook/": 'structure',
-  // "/test/": 'structure',
-
 });
 
